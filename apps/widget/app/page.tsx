d2600b236cf94dd7498e7d084de71bfe284d1840
Jai Lakhmani <jailakhmani12345@gmail.com>
2025-11-03 21:25:32 +0530

    03: clerk authentication

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,20 @@
 "use client";
-import { useQuery } from "convex/react";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/convex/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+// import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUSer = useMutation(api.users.add);
   return (
-    <div className="flex flex-col  items-center justify-center min-h-svh">
-      <div className="max-w-sm w-full  mx-auto">
-
-      {JSON.stringify(users)}
+    <>
+      <div className="flex flex-col  items-center justify-center min-h-svh">
+        {/* <UserButton /> */}
+        <Button onClick={() => addUSer()}> Add New User </Button>
+        <div className="max-w-sm w-full  mx-auto">{JSON.stringify(users)}</div>
       </div>
-      </div>
+    </>
   );
 }
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -9,6 +9,10 @@ export const getMany = query({
 export const add = mutation({
   args: {},
   handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Not authenticated");
+    }
     const userId = ctx.db.insert("users", {
       name: "New User",
     });
