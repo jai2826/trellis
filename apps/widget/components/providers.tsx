aed9aed58a2ab5a539aae5da3bf9799a00ef2c1a
Jai Lakhmani <jailakhmani12345@gmail.com>
2025-11-11 11:50:18 +0530

    11: Widget Screen Router

--- apps/widget/components/providers.tsx
+++ apps/widget/components/providers.tsx
@@ -1,9 +1,23 @@
 "use client";
 
 import * as React from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
+import { Provider as JotaiProvider } from "jotai";
+import {
+	ConvexProvider,
+	ConvexReactClient,
+} from "convex/react";
+const convex = new ConvexReactClient(
+	process.env.NEXT_PUBLIC_CONVEX_URL || ""
+);
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+export function Providers({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<ConvexProvider client={convex}>
+			<JotaiProvider>{children}</JotaiProvider>
+		</ConvexProvider>
+	);
 }
