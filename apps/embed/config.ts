22e320d046e64ab07c5148d0d935db0dba37e800
Jai Lakhmani <jailakhmani12345@gmail.com>
2025-12-16 11:01:59 +0530

    35: Embed Script

---
+++ apps/embed/config.ts
@@ -0,0 +1,8 @@
+export const EMBED_CONFIG = {
+  WIDGET_URL:
+    import.meta.env.VITE_WIDGET_URL ||
+    "http://localhost:3001",
+  DEFAULT_ORG_ID: "org_36hjXrqAYfm9IrGQhqdxC4DhWUM",
+
+  DEFAULT_POSITION: "bottom-right" as const,
+};
